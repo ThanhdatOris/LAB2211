4d3a6092be1f9cdf9ad5cdbb80cbb925350e6587
Sinoo <khoasinno@gmail.com>
2024-08-29 18:33:15 +0700

    v1_change deciaml to money

--- thienan.sql
+++ thienan.sql
@@ -1,25 +1,25 @@
 -- Tạo database
-CREATE DATABASE ThienAnDB;
+CREATE DATABASE VLXDThienAnDB;
 
 go
 
-USE ThienAnDB;
+USE VLXDThienAnDB;
 go
 -- Bảng Role
 CREATE TABLE MyRole (
     id INT PRIMARY KEY IDENTITY(1,1),
-    name VARCHAR(255) NOT NULL
+    name NVARCHAR(255) NOT NULL
 );
 go
 -- Bảng User
 CREATE TABLE MyUser (
     id INT PRIMARY KEY IDENTITY(1,1),
     role_id INT,
-    fullname VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20),
-    address VARCHAR(255),
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    fullname NVARCHAR(255) NOT NULL,
+    phone_number NVARCHAR(20) NOT NULL,
+    address NVARCHAR(255),
+    username NVARCHAR(100) UNIQUE NOT NULL,
+    password NVARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT GETDATE(),
     updated_at DATETIME DEFAULT GETDATE(),
     deleted BIT DEFAULT 0,
@@ -29,15 +29,15 @@ go
 -- Bảng Supplier
 CREATE TABLE Supplier (
     id INT PRIMARY KEY IDENTITY(1,1),
-    display_name VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    phone VARCHAR(20)
+    display_name NVARCHAR(255) NOT NULL,
+    address NVARCHAR(255) NOT NULL,
+    phone NVARCHAR(20) NOT NULL
 );
 go
 -- Bảng Category
 CREATE TABLE Category (
     id INT PRIMARY KEY IDENTITY(1,1),
-    name VARCHAR(255) NOT NULL
+    name NVARCHAR(255) NOT NULL
 );
 go
 -- Bảng Product
@@ -45,17 +45,28 @@ CREATE TABLE MyProduct (
     id INT PRIMARY KEY IDENTITY(1,1),
     category_id INT,
     supplier_id INT,
-    title VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
+    --unit_id INT,
+    title NVARCHAR(255) NOT NULL,
+    unit NVARCHAR(100) NOT NULL,
+    --price MONEY NOT NULL,
     image VARCHAR(255),
     description TEXT,
     created_at DATETIME DEFAULT GETDATE(),
     updated_at DATETIME DEFAULT GETDATE(),
-    status VARCHAR(50) NOT NULL DEFAULT 'available',
+    status NVARCHAR(50) NOT NULL DEFAULT 'Available',
     FOREIGN KEY (category_id) REFERENCES Category(id),
-    FOREIGN KEY (supplier_id) REFERENCES Supplier(id)
+    FOREIGN KEY (supplier_id) REFERENCES Supplier(id),
+    --FOREIGN KEY (unit_id) REFERENCES MyUnit(id)
+
 );
 go
+-- Bảng Unit
+--CREATE TABLE MyUnit (
+--    id INT PRIMARY KEY IDENTITY(1,1),
+--	display_name NVARCHAR(255) NOT NULL,
+--    FOREIGN KEY (supplier_id) REFERENCES Supplier(id)
+--);
+--go
 -- Bảng Gallery
 CREATE TABLE Gallery (
     id INT PRIMARY KEY IDENTITY(1,1),
@@ -67,10 +78,9 @@ go
 -- Bảng Import
 CREATE TABLE Import (
     id INT PRIMARY KEY IDENTITY(1,1),
-    supplier_id INT,
     import_date DATETIME DEFAULT GETDATE(),
     quantity INT NOT NULL,
-    FOREIGN KEY (supplier_id) REFERENCES Supplier(id)
+    status NVARCHAR(50) NOT NULL DEFAULT 'Success',
 );
 go
 -- Bảng Import_Detail
@@ -79,10 +89,10 @@ CREATE TABLE Import_Detail (
     import_id INT,
     product_id INT,
     quantity INT NOT NULL,
-    input_price DECIMAL(10, 2) NOT NULL,
-    output_price DECIMAL(10, 2),
-    total_price DECIMAL(10, 2) NOT NULL,
-    status VARCHAR(50) NOT NULL DEFAULT 'pending',
+    input_price MONEY NOT NULL,
+    output_price MONEY,
+    total_price MONEY NOT NULL,
+    status NVARCHAR(50) NOT NULL DEFAULT 'Pending',
     FOREIGN KEY (import_id) REFERENCES Import(id),
     FOREIGN KEY (product_id) REFERENCES MyProduct(id)
 );
@@ -91,15 +101,15 @@ go
 CREATE TABLE MyOrder (
     id INT PRIMARY KEY IDENTITY(1,1),
     user_id INT,
-    fullname VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20),
-    address VARCHAR(255),
-    note TEXT,
+    fullname NVARCHAR(255) NOT NULL,
+    phone_number NVARCHAR(20),
+    address NVARCHAR(255),
+    note NVARCHAR(255),
     order_date DATETIME DEFAULT GETDATE(),
-    order_status VARCHAR(50) NOT NULL DEFAULT 'pending',
-    total_price DECIMAL(10, 2) NOT NULL,
-    payment_method VARCHAR(100) NOT NULL,
-    payment_status VARCHAR(50) NOT NULL DEFAULT 'unpaid',
+    order_status NVARCHAR(50) NOT NULL DEFAULT 'Pending',
+    total_price MONEY NOT NULL,
+    payment_method NVARCHAR(100) NOT NULL,
+    payment_status NVARCHAR(50) NOT NULL DEFAULT 'Unpaid',
     FOREIGN KEY (user_id) REFERENCES MyUser(id)
 );
 go
@@ -109,10 +119,11 @@ CREATE TABLE Order_Detail (
     order_id INT,
     product_id INT,
     import_id INT,
-    price DECIMAL(10, 2) NOT NULL,
+    price MONEY NOT NULL,
     quantity INT NOT NULL,
     date_output DATETIME,
-    status VARCHAR(50) NOT NULL DEFAULT 'pending',
+    total_price MONEY NOT NULL,
+    status NVARCHAR(50) NOT NULL DEFAULT 'Pending',
     FOREIGN KEY (order_id) REFERENCES MyOrder(id),
     FOREIGN KEY (product_id) REFERENCES MyProduct(id),
     FOREIGN KEY (import_id) REFERENCES Import(id)
